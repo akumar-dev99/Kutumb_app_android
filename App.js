5bda92a8203d3f4abb1dccef271b6cf23c72a952
akumar1999 <akumar1999m@gmail.com>
2022-03-17 23:22:58 +0530

    pull refresh button was implemented

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+
+} from "react-native";
 import Contants from "expo-constants";
 import Home from "./screen/Home";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+
+// const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* <Stack.Navigator>
+        <Stack.Screen
+          name="Intro"
+          component={Home}
+          options={{
+            headerShown: true,
+          }}
+        />
+      </Stack.Navigator> */}
       <Home />
-
-      <StatusBar style="auto" />
     </View>
   );
 }
 
+// export default () => {
+//   return (
+//     <NavigationContainer>
+//       <App />
+//     </NavigationContainer>
+//   );
+// };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#fff',
+    backgroundColor: "#fff",
     marginTop: Contants.statusBarHeight,
   },
 });
--- screen/Home.js
+++ screen/Home.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,49 +7,90 @@ import {
   Image,
   FlatList,
   ScrollView,
+  ActivityIndicator,
+  SafeAreaView,
 } from "react-native";
 import { Card } from "react-native-paper";
 
-export default function Home() {
-  const data = [
-    { id: 1, name: "mukesh", position: "web dev" },
-    { id: 2, name: "mukesh", position: "web development" },
-    { id: 3, name: "mukesh", position: "web dev" },
-    { id: 4, name: "mukesh", position: "web dev" },
-    { id: 5, name: "mukesh", position: "web dev" },
-    { id: 6, name: "mukesh", position: "web dev" },
-    { id: 7, name: "mukesh", position: "web dev" },
-    { id: 8, name: "mukesh", position: "web dev" },
-    { id: 9, name: "mukesh", position: "web dev" },
-    { id: 10, name: "mukesh", position: "web dev" },
-  ];
+const movieURL = "https://gh-trending-api.herokuapp.com/repositories";
 
-  const renderList = (item) => {
-    return (
-      <Card style={styles.card}>
-        <View style={styles.cardView}>
-          <Image
-            style={{ width: 60, height: 60, borderRadius: 50 }}
-            source={{
-              uri: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-            }}
-          />
-          <View style={{ justifyContent: "space-between" }}>
-            <Text style={styles.text}> {item.name}</Text>
-            <Text style={styles.text}> {item.position}</Text>
-          </View>
-        </View>
-      </Card>
-    );
+export default function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [image, setImage] = useState([]);
+
+  //   async function getMoviesAsync() {
+  //     try {
+  //       let response = await fetch(movieURL);
+  //       let json = await response.json();
+  //       setData(json.movies);
+  //       setTitle(json.title);
+  //       setDescription(json.description);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       alert(error);
+  //     }
+  //   }
+
+  const fetchData = () => {
+    fetch(movieURL)
+      .then((response) => response.json()) // get response, convert to json
+      .then((json) => {
+        console.log("refreshed");
+        setData(json);
+        // setImage(json.builtBy)
+      })
+      .catch((error) => alert(error)) // display errors
+      .finally(() => setLoading(false)); // change loading state
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <FlatList
-      data={data}
-      renderItem={({ item }) => {
-        return renderList(item);
-      }}
-      keyExtractor={(item) => `${item.id}`}
-    />
+    <SafeAreaView>
+      {isLoading ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ActivityIndicator size={50} color="#ff9900" />
+        </View>
+      ) : (
+        <View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={{ paddingBottom: 10 }}>
+                <Card style={styles.card}>
+                  <View style={styles.cardView}>
+                    <Image
+                      style={{ width: 60, height: 60, borderRadius: 50 }}
+                      source={{
+                        uri: item.avatar,
+                      }}
+                    />
+
+                    <View style={{ justifyContent: "space-between" }}>
+                      <Text style={styles.text}>{item.username}</Text>
+                      <Text style={styles.text}>{item.repositoryName}</Text>
+                    </View>
+                  </View>
+                </Card>
+              </View>
+            )}
+            onRefresh={() => fetchData()}
+            refreshing={isLoading}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
 
@@ -68,3 +109,31 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
 });
+
+//   const data = [
+//     { id: 1, name: "mukesh", position: "web dev" },
+//     { id: 2, name: "mukesh", position: "web development" },
+//     { id: 3, name: "mukesh", position: "web dev" },
+//     { id: 4, name: "mukesh", position: "web dev" },
+//     { id: 5, name: "mukesh", position: "web dev" },
+//     { id: 6, name: "mukesh", position: "web dev" },
+//     { id: 7, name: "mukesh", position: "web dev" },
+//     { id: 8, name: "mukesh", position: "web dev" },
+//     { id: 9, name: "mukesh", position: "web dev" },
+//     { id: 10, name: "mukesh", position: "web dev" },
+//   ];
+
+//   <Card style={styles.card}>
+//   <View style={styles.cardView}>
+//     <Image
+//       style={{ width: 60, height: 60, borderRadius: 50 }}
+//       source={{
+//         uri: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+//       }}
+//     />
+//     <View style={{ justifyContent: "space-between" }}>
+//       <Text style={styles.text}> Name </Text>
+//       <Text style={styles.text}> Position </Text>
+//     </View>
+//   </View>
+// </Card>
